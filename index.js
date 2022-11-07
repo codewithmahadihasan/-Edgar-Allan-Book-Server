3fcd0170bb9a81ea1d2c47d823d6f8632276829c
loneliness-mahadi <lonelinessmahadi@gmail.com>
2022-11-08 02:57:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running now')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
